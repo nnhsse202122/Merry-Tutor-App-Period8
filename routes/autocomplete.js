a05f6fc1495d538bfee16cd35bc4d693934d8857
Geoff Schmit <gcschmit@gmail.com>
2022-01-04 13:44:22 -0600

    add code from last year

---
+++ routes/autocomplete.js
@@ -0,0 +1,40 @@
+let express = require("express");
+
+const db = require("../db.js");
+
+router = express.Router();
+router.get("/",async (req,res) => {
+    const nameConcats = {
+      "default": [
+        '$name.first', ' ', '$name.last'
+      ],
+      "last,first": [
+        '$name.last', ', ', '$name.first'
+      ]
+    }
+    let results = (await (await db.getUserModel()).aggregate([
+        {
+          '$match': {
+            roles: "tutee"
+          }
+        },
+        {
+          '$addFields': {
+            'fullname': {
+              '$concat': nameConcats[req.query.type]
+            }
+          }
+        }, {
+          '$match': {
+            'fullname': new RegExp(`.*${req.query.input}.*`)
+          }
+        }, {
+          '$project': {
+            'fullname': 1
+          }
+        }
+      ]).exec()).map(x => [x.fullname, String(x._id)])
+    res.json(results);
+});
+
+module.exports = router;
