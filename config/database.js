d351716492ddd5efc902323e1b088cde607d1a02
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-06 08:30:49 -0600

    PLLLZZZZ WORKKKKKK

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
+var protocol = "mongodb+srv";
+var mongoHost = "cluster0.zgj8a.mongodb.net";
+const uri = `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority`;
+
 module.exports = {
 
     //'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
-    'url' : `mongodb://admin:${process.env.MONGO_PASSWORD}@localhost/merry-tutor?retryWrites=true&w=majority` // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
+    'url' : uri
 
 };
