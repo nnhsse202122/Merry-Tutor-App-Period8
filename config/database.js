8f3e71ea63a1fc74ed93e10f2a45f38aa4e75b5d
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-06 08:40:44 -0600

    I HOPE THIS WORKS

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-var protocol = "mongodb+srv";
-var mongoHost = "cluster0.zgj8a.mongodb.net";
+var protocol = 'mongodb+srv';
+var mongoHost = 'cluster0.zgj8a.mongodb.net';
 const uri = `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority`;
 
 module.exports = {
