efdd552e6f027573f1c0cd09b405cadf54e512f1
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-05 22:28:37 -0600

    stuff2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
+    //'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
+    'url' : 'mongodb://rsrao@stu.naperville203.org:daz1248daz@mongo.onmodulus.net:8080' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
 
 };
