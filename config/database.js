1730282c90ac71e01c0b184a945bcd6d5e8ab325
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-05 22:37:11 -0600

    LETSSSSGOOOITWORKED

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     //'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
-    'url' : 'mongodb://admin:DZX7ZRUrhe6TO7SR@localhost/merry-tutor?retryWrites=true&w=majority' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
+    'url' : `mongodb://admin:${}@localhost/merry-tutor?retryWrites=true&w=majority` // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
 
 };
