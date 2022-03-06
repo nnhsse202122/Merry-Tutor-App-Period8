37fd8ac13d643e5dc8d3225afb216f55bf177d27
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-05 22:31:58 -0600

    stuff3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     //'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
-    'url' : 'mongodb://rsrao@stu.naperville203.org:daz1248daz@mongo.onmodulus.net:8080' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
+    'url' : `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority` // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:<port>
 
 };
