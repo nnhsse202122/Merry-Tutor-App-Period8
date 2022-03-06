1f9eb167d497a1432ba518349af186da88af4d49
rsrao-1 <rsrao@stu.naperville203.org>
2022-03-06 08:49:09 -0600

    UGG THIS ISNT WORKING

--- config/database.js
+++ config/database.js
@@ -1,5 +1,13 @@
-var protocol = 'mongodb+srv';
-var mongoHost = 'cluster0.zgj8a.mongodb.net';
+if(process.env.PRODUCTION) {
+	console.log("Running on production server...");
+	var protocol = "mongodb";
+	var mongoHost = "localhost";
+}
+else {
+	console.log("Running for development...");
+	var protocol = "mongodb+srv";
+	var mongoHost = "cluster0.zgj8a.mongodb.net";
+}
 const uri = `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority`;
 
 module.exports = {
