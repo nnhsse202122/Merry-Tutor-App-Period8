058b50b625ce08494dc7cd3f4a9a37ca6828995a
rsrao-1 <rsrao@stu.naperville203.org>
2022-05-04 15:08:38 -0500

    plz work

--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,6 @@
+//const express = require("express");
+//const app = express();
+
 const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 var shownSlideId = "base-login";
 
@@ -23,6 +26,5 @@ async function doRegister(){
     })
     var user = await res.json();
     console.log(user);
-    
+    res.render('../views/registrationconfirmation');
 }
-   
