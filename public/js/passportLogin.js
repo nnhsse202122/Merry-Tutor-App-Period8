c64f1d4b7c278f578563827aa86561320ef37480
OurGreatLeaderEason <58402054+OurGreatLeaderEason@users.noreply.github.com>
2022-05-03 21:43:00 -0500

    Login button works now

--- public/js/passportLogin.js
+++ public/js/passportLogin.js
@@ -1,7 +1,8 @@
 
+document.querySelector("#auth-login").addEventListener("click", doLogin);
 
-document.querySelector("#auth-login").addEventListener("click",doLogin());
-
-async function deLogin(){
-    
+async function doLogin(){
+    let PassportUserData={};
+    newPassportUserData.email=document.querySelector("#signup-info input[name=username]").value;
+    newPassportUserData.password=document.querySelector("#signup-info input[name=password]").value;
 }
