d5a91444bbb59cdd98edf835f5a8f63d54ea6492
OurGreatLeaderEason <58402054+OurGreatLeaderEason@users.noreply.github.com>
2022-05-03 21:56:44 -0500

    ???

--- public/js/passportLogin.js
+++ public/js/passportLogin.js
@@ -3,6 +3,8 @@ document.querySelector("#auth-login").addEventListener("click", doLogin);
 
 async function doLogin(){
     let PassportUserData={};
-    newPassportUserData.email=document.querySelector("#signup-info input[name=username]").value;
-    newPassportUserData.password=document.querySelector("#signup-info input[name=password]").value;
+    PassportUserData.email=document.querySelector("#nigger input[name=email]").value;
+    PassportUserData.password=document.querySelector("#nigger input[name=password]").value;
+    alert(PassportUserData.email);
+    alert(PassportUserData.password);
 }
